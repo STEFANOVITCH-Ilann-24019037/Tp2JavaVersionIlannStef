ecaa89fa552238596c5b5eb898e3b5b7af9644d5
ilannstefanovitch <ilann.stefanovitch@etu.univ-amu.fr>
2025-01-23 15:02:20 +0100

    Rajout de la class Author et Book(qui n'est pas fini) et modif des exo (il y a plus que 1 fichier pour tout les exo et ce fichier fait appelle a de fonction Exo1 et exo....)

---
+++ fr/IlannStefanovitch/Tp2/Exo.java
@@ -0,0 +1,54 @@
+package fr.IlannStefanovitch.Tp2;
+
+public class Exo {
+
+    public static void main(String[] args) {
+       //exo1();
+       exo2();
+    }
+    public static void exo1 () {
+        // Test Constructors and toString()
+        Circle c1 = new Circle(1.1);
+        System.out.println(c1);   // toString()
+        Circle c2 = new Circle(); // default constructor
+        System.out.println(c2);
+
+        // Test setter and getter
+        c1.setRadius(2.2);
+        System.out.println(c1);      // toString()
+        System.out.println("radius is: " + c1.getRadius());
+
+        // Test getArea() and getCircumference()
+        System.out.printf("area is: %.2f%n", c1.getArea());
+        System.out.printf("circumference is: %.2f%n", c1.getCircumference());
+    }
+    public static void exo2 () {
+        Author ahTeck = new Author("Tan Ah Teck", "ahteck@nowhere.com", 'm'); // Test the constructor
+        System.out.println(ahTeck);  // Test toString()
+        ahTeck.setEmail("paulTan@nowhere.com");  // Test setter
+        System.out.println("name is: " + ahTeck.getName());     // Test getter
+        System.out.println("email is: " + ahTeck.getEmail());   // Test getter
+        System.out.println("gender is: " + ahTeck.getGender()); // Test getter
+        System.out.println(" ");
+
+        Book dummyBook = new Book("Java for dummy", ahTeck, 19.95, 99);  // Test Book's Constructor
+        System.out.println(dummyBook);  // Test Book's toString()
+
+        // Test Getters and Setters
+        dummyBook.setPrice(29.95);
+        dummyBook.setQty(28);
+        System.out.println("name is: " + dummyBook.getName());
+        System.out.println("price is: " + dummyBook.getPrice());
+        System.out.println("qty is: " + dummyBook.getQty());
+        System.out.println("Author is: " + dummyBook.getAuthor());  // Author's toString()
+        System.out.println("Author's name is: " + dummyBook.getAuthor().getName());
+        System.out.println("Author's email is: " + dummyBook.getAuthor().getEmail());
+
+        // Use an anonymous instance of Author to construct a Book instance
+        Book anotherBook = new Book("more Java",
+                new Author("Paul Tan", "paul@somewhere.com", 'm'), 29.95);
+        System.out.println(anotherBook);  // toString()
+
+        System.out.println("name of the Author is : " + anotherBook.getAuthor().getName() + " and is email is : " + anotherBook.getAuthor().getEmail());
+    }
+}
